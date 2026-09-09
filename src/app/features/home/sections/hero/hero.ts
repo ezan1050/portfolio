2327c8b015d6b164676d6ef905572987e9ffa038
ezan bhatti <ezan1050@gmail.com>
2026-09-09 10:42:36 +0200

    feat: add matrix scramble effect on hero name

--- src/app/features/home/sections/hero/hero.ts
+++ src/app/features/home/sections/hero/hero.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit, OnDestroy, PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.html',
-  styleUrl: './hero.css',
+  styleUrl: './hero.css'
 })
-export class Hero {
+export class Hero implements OnInit, OnDestroy {
+  private platformId = inject(PLATFORM_ID);
 
+  private readonly targetName = 'Ezan Bhatti';
+  private readonly chars = 'Z0123456789ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ';
+
+  displayName = signal('');
+  private intervalId: any;
+
+  ngOnInit() {
+    if (!isPlatformBrowser(this.platformId)) {
+      // Côté serveur : afficher directement le nom final
+      this.displayName.set(this.targetName);
+      return;
+    }
+
+    let revealed = 0;
+
+    this.intervalId = setInterval(() => {
+      let result = '';
+
+      for (let i = 0; i < this.targetName.length; i++) {
+        if (i < revealed) {
+          result += this.targetName[i];
+        } else if (this.targetName[i] === ' ') {
+          result += ' ';
+        } else {
+          result += this.chars[Math.floor(Math.random() * this.chars.length)];
+        }
+      }
+
+      this.displayName.set(result);
+
+      revealed++;
+
+      if (revealed > this.targetName.length) {
+        clearInterval(this.intervalId);
+        this.displayName.set(this.targetName);
+      }
+    }, 100);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
