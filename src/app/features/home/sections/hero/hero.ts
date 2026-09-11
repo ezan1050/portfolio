8b3c2d82c8ffedaf79d883e00aa1db97a12eefd1
ezan bhatti <ezan1050@gmail.com>
2026-09-11 11:03:13 +0200

    feat: add terminal typing effect on hero role

--- src/app/features/home/sections/hero/hero.ts
+++ src/app/features/home/sections/hero/hero.ts
@@ -10,22 +10,35 @@ import { isPlatformBrowser } from '@angular/common';
 export class Hero implements OnInit, OnDestroy {
   private platformId = inject(PLATFORM_ID);
 
+  // --- Effet Matrix (nom) ---
   private readonly targetName = 'Ezan Bhatti';
-  private readonly chars = 'Z0123456789ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ';
-
+  private readonly chars = 'ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ';
   displayName = signal('');
-  private intervalId: any;
+  private scrambleInterval: any;
+
+  // --- Effet terminal (rôle) ---
+  private readonly roles = ['Software Developer', 'Cloud Administrator', 'IT Helpdesk'];
+  displayRole = signal('');
+  private roleIndex = 0;
+  private charIndex = 0;
+  private isDeleting = false;
+  private terminalTimeout: any;
 
   ngOnInit() {
     if (!isPlatformBrowser(this.platformId)) {
-      // Côté serveur : afficher directement le nom final
       this.displayName.set(this.targetName);
+      this.displayRole.set(this.roles[0]);
       return;
     }
 
+    this.startScramble();
+    this.typeRole();
+  }
+
+  private startScramble() {
     let revealed = 0;
 
-    this.intervalId = setInterval(() => {
+    this.scrambleInterval = setInterval(() => {
       let result = '';
 
       for (let i = 0; i < this.targetName.length; i++) {
@@ -39,19 +52,42 @@ export class Hero implements OnInit, OnDestroy {
       }
 
       this.displayName.set(result);
-
       revealed++;
 
       if (revealed > this.targetName.length) {
-        clearInterval(this.intervalId);
+        clearInterval(this.scrambleInterval);
         this.displayName.set(this.targetName);
       }
-    }, 100);
+    }, 80);
+  }
+
+  private typeRole() {
+    const currentRole = this.roles[this.roleIndex];
+
+    if (this.isDeleting) {
+      this.charIndex--;
+      this.displayRole.set(currentRole.substring(0, this.charIndex));
+    } else {
+      this.charIndex++;
+      this.displayRole.set(currentRole.substring(0, this.charIndex));
+    }
+
+    let delay = this.isDeleting ? 50 : 100;
+
+    if (!this.isDeleting && this.charIndex === currentRole.length) {
+      delay = 1500;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.charIndex === 0) {
+      this.isDeleting = false;
+      this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+      delay = 300;
+    }
+
+    this.terminalTimeout = setTimeout(() => this.typeRole(), delay);
   }
 
   ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
+    if (this.scrambleInterval) clearInterval(this.scrambleInterval);
+    if (this.terminalTimeout) clearTimeout(this.terminalTimeout);
   }
 }
