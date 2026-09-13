0c7a6c9bd4157892068eb5fb2db1f5ccd683df93
ezan1050 <ezan1050@gmail.com>
2026-09-13 11:41:15 +0200

    feat: add experience timeline section (#8)

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -4,10 +4,11 @@ import { Skills } from '../sections/skills/skills';
 import { About } from '../sections/about/about';
 import { Projects } from '../sections/projects/projects';
 import { Certifications } from "../sections/certifications/certifications";
+import { Experience } from "../sections/experience/experience";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Skills, About, Projects, Certifications],
+  imports: [Hero, Skills, About, Experience, Projects, Certifications],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
